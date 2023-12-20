2b2664b4cd63306bad773e76b4d494a7dc9888f6
ponselvajeganathan <ponselva.jeganathan@syncfusion.com>
2023-12-20 15:29:41 +0530

    sample(381594): sample changes

---
+++ Javascript url adaptor with CRUD/RemoteSave/Controllers/HomeController.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using url.Models;
+using RemoteSave.Models;
+using Syncfusion.EJ2.Base;
+using System.Collections;
+using Syncfusion.EJ2.Linq;
+using Syncfusion.EJ2.Navigations;
+
+namespace RemoteSave.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+          
+        public ActionResult Index()
+        {
+            
+            
+            
+            return View();
+        }
+        public ActionResult UrlDatasource(DataManagerRequest dm)
+        {
+            if (TreeData.tree.Count == 0)
+                TreeData.GetTree();
+            IEnumerable DataSource = TreeData.tree;
+            IEnumerable src =null;
+            IEnumerable dest = null;
+            IEnumerable aggregate=null;
+
+            DataOperations operation = new DataOperations();
+            if (dm.Where != null && dm.Where.Count > 0)
+            {
+                DataSource = operation.PerformFiltering(DataSource, dm.Where, "and");
+            }
+            List<string> str = new List<string>();
+            
+
+            if (dm.Sorted != null && dm.Sorted.Count > 0)
+            {
+                DataSource = operation.PerformSorting(DataSource, dm.Sorted);
+            }
+            var count = DataSource.ToList<TreeData>().Count();
+            if (dm.Skip != 0)
+            {
+                DataSource = operation.PerformSkip(DataSource, dm.Skip);   //Paging
+            }
+            if (dm.Take != 0)
+            {
+                DataSource = operation.PerformTake(DataSource, dm.Take);
+            }
+           
+
+
+
+                return dm.RequiresCounts ? Json(new { result = DataSource, count = count }) : Json(DataSource);
+        }
+
+      
+
+        public ActionResult Update(TreeData value)
+        {
+            var val = TreeData.tree.Where(ds => ds.TaskID == value.TaskID).FirstOrDefault();
+            val.TaskName = value.TaskName;
+            val.StartDate = value.StartDate;
+            val.Duration = value.Duration;
+            val.Priority = value.Priority;
+            val.Progress = value.Progress;
+            return Json(value);
+
+        }
+
+        public void Insert(TreeData value, int relationalKey)
+        {
+            var i = 0;
+            for (; i < TreeData.tree.Count; i++)
+            {
+                if (TreeData.tree[i].TaskID == relationalKey)
+                {
+                    break;
+
+                }
+            }
+            i += FindChildRecords(relationalKey);
+            TreeData.tree.Insert(i +1, value);
+        }
+        public int FindChildRecords(int? id)
+        {
+            var count = 0;
+            for (var i = 0; i < TreeData.tree.Count; i++)
+            {
+                if (TreeData.tree[i].ParentItem == id)
+                {
+                    count++;
+                    count += FindChildRecords(TreeData.tree[i].TaskID);
+                }
+            }
+            return count;
+        }
+        
+        public void Delete(int key)
+        {
+            TreeData.tree.Remove(TreeData.tree.Where(ds => ds.TaskID == key).FirstOrDefault());
+        }
+
+        // Remove method (batchUrl) will be triggered when we delete parent record.
+
+        public void Remove(List<TreeData> changed, List<TreeData> added, List<TreeData> deleted)
+        {
+            for (var i = 0; i < deleted.Count; i++)
+            {
+                TreeData.tree.Remove(TreeData.tree.Where(ds => ds.TaskID == deleted[i].TaskID).FirstOrDefault());
+            }
+        }
+
+
+        public class CustomBind : TreeData
+        {
+            public TreeData parentItem;
+        }
+
+        public class ICRUDModel
+        {
+            public TreeData value;
+            public int key;
+            public string action;
+        }
+
+    }
+   
+
+
+        public class TreeData
+        {
+            public static List<TreeData> tree = new List<TreeData>();
+            [System.ComponentModel.DataAnnotations.Key]
+            public int TaskID { get; set; }
+            public string TaskName { get; set; }
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+            public String Progress { get; set; }
+            public String Priority { get; set; }
+            public int Duration { get; set; }
+            public int? ParentItem { get; set; }
+            public bool? isParent { get; set; }
+            public TreeData() { }
+            public static List<TreeData> GetTree()
+            {
+                if (tree.Count == 0)
+                {
+                    int root = -1;
+                for (var t = 1; t <= 5; t++)
+                {
+                    Random ran = new Random();
+                        string math = (ran.Next() % 3) == 0 ? "High" : (ran.Next() % 2) == 0 ? "Release Breaker" : "Critical";
+                        string progr = (ran.Next() % 3) == 0 ? "Started" : (ran.Next() % 2) == 0 ? "Open" : "In Progress";
+                        root++;
+                        int rootItem = tree.Count + root + 1;
+                        tree.Add(new TreeData() { TaskID = rootItem, TaskName = "Parent Task " + rootItem.ToString(), StartDate = new DateTime(1992, 06, 07), EndDate = new DateTime(1994, 08, 25), isParent = true, ParentItem = null, Progress = progr, Priority = math, Duration = (ran.Next(1, 50) *5) });
+                        int parent = tree.Count;
+                        for (var c = 0; c < 3; c++)
+                        {
+                            root++;
+                            string val = ((parent + c + 1) % 3 == 0) ? "Low" : "Critical";
+                            int parn = parent + c + 1;
+                            progr = (ran.Next() % 3) == 0 ? "In Progress" : (ran.Next() % 2) == 0 ? "Open" : "Validated";
+                            int iD = tree.Count + root + 1;
+                            tree.Add(new TreeData() { TaskID = iD, TaskName = "Child Task " + iD.ToString(), StartDate = new DateTime(1992, 06, 07), EndDate = new DateTime(1994, 08, 25), isParent = false, ParentItem = rootItem, Progress = progr, Priority = val, Duration = ran.Next(1, 50) });
+                    if ((((parent + c + 1) % 2) == 0))
+                    {
+                        int immParent = tree.Count;
+                        for (var s = 0; s < 2; s++)
+                        {
+                            root++;
+                            string Prior = (immParent % 2 == 0) ? "Validated" : "Normal";
+                            tree.Add(new TreeData() { TaskID = tree.Count + root + 1, TaskName = "Sub Task " + (tree.Count + root + 1).ToString(), StartDate = new DateTime(1992, 06, 07), EndDate = new DateTime(1994, 08, 25), isParent = false, ParentItem = iD, Progress = (immParent % 2 == 0) ? "On Progress" : "Closed", Priority = Prior, Duration = ran.Next(1, 50) });
+                        }
+                    }
+                     }
+                }
+                }
+                return tree;
+            }
+        }
+    }
---
+++ Javascript url adaptor with CRUD/RemoteSave/Models/TreeGridItems.cs
@@ -0,0 +1,453 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RemoteSave.Models
+{
+    public class TreeGridItems
+    {
+        public TreeGridItems() { }
+        public int TaskId { get; set; }
+
+        public string TaskName { get; set; }
+
+        public DateTime USAActiveDate { get; set; }
+
+        public DateTime USAInactiveDate { get; set; }
+
+        public int Duration { get; set; }
+
+        public int Progress { get; set; }
+        public string Priority { get; set; }
+        public bool Approved { get; set; }
+
+        public DateTime InternationalActiveDate { get; set; }
+        public DateTime InternationalInactiveDate { get; set; }
+
+        public List<TreeGridItems> Children { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public static List<TreeGridItems> GetTreeData()
+        {
+            List<TreeGridItems> BusinessObjectCollection = new List<TreeGridItems>();
+
+            TreeGridItems Record1 = null;
+
+            Record1 = new TreeGridItems()
+            {
+                TaskId = 1,
+                TaskName = "Planning",
+                USAActiveDate = new DateTime(2016, 06, 07),
+                USAInactiveDate = new DateTime(2021, 08, 25),
+                Progress = 100,
+                Duration = 5,
+                Priority = "Normal",
+                Approved = false,
+                InternationalActiveDate = new DateTime(2017, 02, 03),
+                InternationalInactiveDate = new DateTime(2017, 02, 07),
+                Children = new List<TreeGridItems>(),
+            };
+
+            TreeGridItems Child1 = new TreeGridItems()
+            {
+                TaskId = 2,
+                TaskName = "Plan timeline",
+                USAActiveDate = new DateTime(2016, 06, 07),
+                USAInactiveDate = new DateTime(2021, 08, 25),
+                Progress = 100,
+                Duration = 5,
+                Priority = "Normal",
+                Approved = false,
+                InternationalActiveDate = new DateTime(2017, 02, 03),
+                InternationalInactiveDate = new DateTime(2017, 02, 07),
+            };
+
+            TreeGridItems Child2 = new TreeGridItems()
+            {
+                TaskId = 3,
+                TaskName = "Plan budget",
+                USAActiveDate = new DateTime(2016, 06, 07),
+                USAInactiveDate = new DateTime(2021, 08, 25),
+                Duration = 5,
+                Priority = "Critical",
+                Progress = 100,
+                Approved = true,
+                InternationalActiveDate = new DateTime(2017, 02, 03),
+                InternationalInactiveDate = new DateTime(2017, 02, 07),
+            };
+
+            TreeGridItems Child3 = new TreeGridItems()
+            {
+                TaskId = 4,
+                TaskName = "Allocate resources",
+                USAActiveDate = new DateTime(2016, 06, 07),
+                USAInactiveDate = new DateTime(2021, 08, 25),
+                Duration = 5,
+                Progress = 100,
+                Priority = "Critical",
+                InternationalActiveDate = new DateTime(2017, 02, 03),
+                InternationalInactiveDate = new DateTime(2017, 02, 07),
+                Approved = false
+            };
+
+            TreeGridItems Child4 = new TreeGridItems()
+            {
+                TaskId = 5,
+                TaskName = "Planning complete",
+                USAActiveDate = new DateTime(2021, 08, 25),
+                USAInactiveDate = new DateTime(2021, 08, 25),
+                Duration = 3,
+                Progress = 25,
+                Priority = "Low",
+                InternationalActiveDate = new DateTime(2017, 02, 07),
+                InternationalInactiveDate = new DateTime(2017, 02, 07),
+                Approved = true
+            };
+            TreeGridItems Child5 = new TreeGridItems()
+            {
+                TaskId = 7,
+                TaskName = "Software Specification",
+                USAActiveDate = new DateTime(2021, 08, 25),
+                USAInactiveDate = new DateTime(2024, 06, 27),
+                Duration = 3,
+                Progress = 60,
+                Priority = "Normal",
+                InternationalActiveDate = new DateTime(2017, 02, 10),
+                InternationalInactiveDate = new DateTime(2017, 02, 12),
+                Approved = false
+            };
+
+
+            Record1.Children.Add(Child1);
+            Record1.Children.Add(Child2);
+            Record1.Children.Add(Child3);
+            Record1.Children.Add(Child4);
+            Record1.Children.Add(Child5);
+
+            BusinessObjectCollection.Add(Record1);
+           
+
+            return BusinessObjectCollection;
+        }
+
+        public static List<object> GetTemplateData()
+        {
+            List<Object> DataCollection = new List<object>();
+            List<Object> Child111 = new List<object>();
+            Child111.Add(new
+            {
+                Name = "Andrew Fuller",
+                FullName = "AndrewFuller",
+                Designation = "Sales Representative",
+                EmployeeID = "4",
+                EmpID = "EMP045",
+                Address = "14 Garrett Hill, London",
+                Contact = "(71) 555-4848",
+                Country = "UK",
+                DOB = new DateTime(1980, 9, 20)
+            });
+            Child111.Add(new
+            {
+                Name = "Anne Dodsworth",
+                FullName = "AnneDodsworth",
+                Designation = "Sales Representative",
+                EmployeeID = "5",
+                EmpID = "EMP091",
+                Address = "4726 - 11th Ave. N.E., Seattle",
+                Contact = "(206) 555-1189",
+                Country = "USA",
+                DOB = new DateTime(1989, 10, 19)
+            });
+            Child111.Add(new
+            {
+                Name = "Michael Suyama",
+                FullName = "MichaelSuyama",
+                Designation = "Sales Representative",
+                EmployeeID = "6",
+                EmpID = "EMP110",
+                Address = "Coventry House Miner Rd., London",
+                Contact = "(71) 555-3636",
+                Country = "UK",
+                DOB = new DateTime(1987, 11, 02)
+            });
+            Child111.Add(new
+            {
+                Name = "Janet Leverling",
+                FullName = "JanetLeverling",
+                Designation = "Sales Coordinator",
+                EmployeeID = "7",
+                EmpID = "EMP131",
+                Address = "Edgeham Hollow Winchester Way, London",
+                Contact = "(71) 555-3636",
+                Country = "UK",
+                DOB = new DateTime(1090, 11, 06)
+            });
+            List<Object> Child11 = new List<object>();
+            Child11.Add(new
+            {
+                Name = "Nancy Davolio",
+                FullName = "NancyDavolio",
+                Designation = "Marketing Executive",
+                EmployeeID = "3",
+                EmpID = "EMP035",
+                Address = "4110 Old Redmond Rd., Redmond",
+                Contact = "(206) 555-8122",
+                Country = "USA",
+                DOB = new DateTime(1966, 3, 19),
+                Children = Child111
+            });
+            List<Object> Child112 = new List<object>();
+            Child112.Add(new
+            {
+                Name = "Margaret Peacock",
+                FullName = "MargaretPeacock",
+                Designation = "Sales Representative",
+                EmployeeID = "9",
+                EmpID = "EMP213",
+                Address = "4726 - 11th Ave. N.E., California",
+                Contact = "(206) 555-1989",
+                Country = "USA",
+                DOB = new DateTime(1986, 1, 21)
+            });
+            Child112.Add(new
+            {
+                Name = "Steven Buchanan",
+                FullName = "StevenBuchanan",
+                Designation = "Sales Representative",
+                EmployeeID = "11",
+                EmpID = "EMP197",
+                Address = "200 Lincoln Ave, Salinas, CA 93901",
+                Contact = "(831) 758-7408",
+                Country = "USA",
+                DOB = new DateTime(1987, 3, 23)
+            });
+            Child112.Add(new
+            {
+                Name = "Tedd Lawson",
+                FullName = "TeddLawson",
+                Designation = "Sales Representative",
+                EmployeeID = "12",
+                EmpID = "EMP167",
+                Address = "200 Lincoln Ave, Salinas, CA 93901",
+                Contact = "(831) 758-7368",
+                Country = "USA",
+                DOB = new DateTime(1989, 8, 9)
+            });
+            Child11.Add(new
+            {
+                Name = "Romey Wilson",
+                FullName = "RomeyWilson",
+                Designation = "Sales Executive",
+                EmployeeID = "8",
+                EmpID = "EMP039",
+                Address = "7 Houndstooth Rd., London",
+                Contact = "(71) 555-3690",
+                Country = "UK",
+                DOB = new DateTime(1980, 2, 2),
+                Children = Child112
+            });
+            List<Object> Child1 = new List<object>();
+            Child1.Add(new
+            {
+                Name = "David william",
+                FullName = "DavidWilliam",
+                Designation = "Vice President",
+                EmployeeID = "2",
+                EmpID = "EMP004",
+                Address = "722 Moss Bay Blvd., Kirkland",
+                Contact = "(206) 555-3412",
+                Country = "USA",
+                DOB = new DateTime(1971, 5, 20),
+                Children = Child11
+            });
+            DataCollection.Add(new
+            {
+                Name = "Robert King",
+                FullName = "RobertKing",
+                Designation = "Chief Executive Officer",
+                EmployeeID = "1",
+                EmpID = "EMP001",
+                Address = "507 - 20th Ave. E.Apt. 2A, Seattle",
+                Contact = "(206) 555-9857",
+                Country = "USA",
+                DOB = new DateTime(1963, 2, 15),
+                Children = Child1
+            });
+            return DataCollection;
+        }
+        public static List<TreeGridItems> GetSelfData()
+        {
+            List<TreeGridItems> BusinessObjectCollection = new List<TreeGridItems>();
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 1,
+                TaskName = "Parent Task 1",
+                USAActiveDate = new DateTime(2017, 10, 23),
+                USAInactiveDate = new DateTime(2017, 11, 27),
+                Duration = 10,
+                Progress = 70,
+                Priority = "Critical",
+                ParentId = null
+            });
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 2,
+                TaskName = "Child task 1",
+                USAActiveDate = new DateTime(2017, 10, 23),
+                USAInactiveDate = new DateTime(2017, 12, 25),
+                Duration = 4,
+                Progress = 80,
+                Priority = "Low",
+                ParentId = 1
+            });
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 3,
+                TaskName = "Child Task 2",
+                USAActiveDate = new DateTime(2017, 10, 24),
+                USAInactiveDate = new DateTime(2018, 03, 19),
+                Duration = 5,
+                Progress = 65,
+                Priority = "High",
+                ParentId = 1
+            });
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 4,
+                TaskName = "Child task 3",
+                USAActiveDate = new DateTime(2017, 10, 25),
+                USAInactiveDate = new DateTime(2018, 10, 26),
+                Duration = 6,
+                Progress = 77,
+                Priority = "Low",
+                ParentId = 1
+            });
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 5,
+                TaskName = "Parent Task 2",
+                USAActiveDate = new DateTime(2017, 10, 23),
+                USAInactiveDate = new DateTime(2018, 10, 27),
+                Duration = 10,
+                Progress = 70,
+                Priority = "Breaking",
+                ParentId = null
+            });
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 6,
+                TaskName = "Child task 1",
+                USAActiveDate = new DateTime(2017, 10, 23),
+                USAInactiveDate = new DateTime(2017, 12, 19),
+                Duration = 4,
+                Progress = 80,
+                Priority = "Normal",
+                ParentId = 5
+            });
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 7,
+                TaskName = "Child Task 2",
+                USAActiveDate = new DateTime(2017, 10, 24),
+                USAInactiveDate = new DateTime(2017, 11, 20),
+                Duration = 5,
+                Priority = "High",
+                Progress = 65,
+                ParentId = 5
+            });
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 8,
+                TaskName = "Child task 3",
+                USAActiveDate = new DateTime(2017, 10, 25),
+                USAInactiveDate = new DateTime(2018, 11, 20),
+                Duration = 6,
+                Priority = "High",
+                Progress = 77,
+                ParentId = 5
+            });
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 9,
+                TaskName = "Child task 4",
+                USAActiveDate = new DateTime(2017, 10, 25),
+                USAInactiveDate = new DateTime(2018, 03, 19),
+                Duration = 6,
+                Progress = 77,
+                Priority = "Low",
+                ParentId = 5
+            });
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 10,
+                TaskName = "Parent Task 3",
+                USAActiveDate = new DateTime(2017, 10, 23),
+                USAInactiveDate = new DateTime(2018, 03, 25),
+                Duration = 10,
+                Progress = 70,
+                Priority = "Normal",
+                ParentId = null
+            });
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 11,
+                TaskName = "Child task 1",
+                USAActiveDate = new DateTime(2017, 10, 23),
+                Duration = 4,
+                USAInactiveDate = new DateTime(2017, 10, 30),
+                Priority = "Low",
+                Progress = 80,
+                ParentId = 10
+            });
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 12,
+                TaskName = "Child Task 2",
+                USAActiveDate = new DateTime(2017, 10, 24),
+                USAInactiveDate = new DateTime(2018, 11, 19),
+                Duration = 5,
+                Progress = 65,
+                Priority = "Critical",
+                ParentId = 10
+            });
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 13,
+                TaskName = "Child task 3",
+                USAActiveDate = new DateTime(2017, 10, 25),
+                USAInactiveDate = new DateTime(2017, 11, 10),
+                Duration = 6,
+                Progress = 77,
+                Priority = "Critical",
+                ParentId = 10
+            });
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 14,
+                TaskName = "Child task 4",
+                USAActiveDate = new DateTime(2017, 10, 25),
+                USAInactiveDate = new DateTime(2017, 11, 12),
+                Duration = 6,
+                Progress = 77,
+                Priority = "High",
+                ParentId = 10
+            });
+            BusinessObjectCollection.Add(new TreeGridItems()
+            {
+                TaskId = 15,
+                TaskName = "Child task 5",
+                USAActiveDate = new DateTime(2017, 10, 25),
+                USAInactiveDate = new DateTime(2017, 11, 23),
+                Duration = 6,
+                Progress = 77,
+                Priority = "Low",
+                ParentId = 10
+            });
+
+            return BusinessObjectCollection;
+        }
+
+    }
+}
